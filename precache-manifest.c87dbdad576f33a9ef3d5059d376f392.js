f4292aaa3540e51811179c744f09bdbb75e76f66
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-08-21 15:32:21 +0300

    Updates

---
+++ precache-manifest.c87dbdad576f33a9ef3d5059d376f392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537c1e882494fa4b3562aa70209faf39",
+    "url": "/cells-live-and-die/index.html"
+  },
+  {
+    "revision": "7b54ef97d8d8a227c61f",
+    "url": "/cells-live-and-die/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "af3492e1f06dd93c3101",
+    "url": "/cells-live-and-die/static/js/2.d7f7936b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cells-live-and-die/static/js/2.d7f7936b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b54ef97d8d8a227c61f",
+    "url": "/cells-live-and-die/static/js/main.05a27fde.chunk.js"
+  },
+  {
+    "revision": "cf0b510439d0ef2534e1",
+    "url": "/cells-live-and-die/static/js/runtime-main.923b1616.js"
+  }
+]);
