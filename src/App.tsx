9cfd69406872d0a16e0a7111a6b7968c535f7d99
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-08-21 15:25:47 +0300

    Createt app cell live and die

---
+++ src/App.tsx
@@ -0,0 +1,135 @@
+import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
+
+const numRows = 50;
+const numCols = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+// Function generate grid
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+
+const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 100);
+  }, []);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
+        style={{
+          width: 50,
+          height: 40,
+          backgroundColor: "red",
+          border: "solid 1px black",
+          margin: "auto",
+          marginLeft: 750,
+        }}
+      >
+        {running ? "stop" : "start"}
+      </button>
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid());
+        }}
+        style={{
+          width: 50,
+          height: 40,
+          backgroundColor: "red",
+          border: "solid 1px black",
+          margin: `0, auto`,
+        }}
+      >
+        clear
+      </button>
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          gridTemplateColumns: `repeat(${numCols}, 10px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: grid[i][k] ? "black" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+
+export default App;
